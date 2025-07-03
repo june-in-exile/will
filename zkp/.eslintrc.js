2cd0521c25d5278f9097d7da6f9389f692bdd813
June <df41022@gmail.com>
2025-07-03 12:43:54 +0800

    update .eslintrc.js

--- zkp/.eslintrc.js
+++ zkp/.eslintrc.js
@@ -1,10 +1,30 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
+  extends: ["eslint:recommended", "@typescript-eslint/recommended"],
+  plugins: ["@typescript-eslint"],
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: "module",
   },
-  plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  rules: {},
+  env: {
+    node: true,
+    jest: true,
+    es6: true,
+  },
+  rules: {
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-non-null-assertion": "warn",
+    "no-console": "off",
+    "prefer-const": "error",
+    "no-var": "error",
+  },
+  ignorePatterns: [
+    "node_modules/",
+    "circuits/*/build/",
+    "circuits/*/keys/",
+    "circuits/*/proofs/",
+  ],
 };
