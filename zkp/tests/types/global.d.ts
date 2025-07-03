6838ff7014bca726283a276d1bea8b85cc1f2bc1
June <df41022@gmail.com>
2025-07-03 14:07:45 +0800

    update compile function

--- zkp/tests/types/global.d.ts
+++ zkp/tests/types/global.d.ts
@@ -81,6 +81,18 @@ declare global {
   }
 
   const circom_tester: CircomTester.CircuitTester;
+
+  type Optimization = 0 | 1 | 2;
+
+  type CurveName =
+    | 'bn128'
+    | 'bls12377'
+    | 'bls12381'
+    | 'goldilocks'
+    | 'grumpkin'
+    | 'pallas'
+    | 'secq256r1'
+    | 'vesta';
 }
 
-export {};
+export { };
