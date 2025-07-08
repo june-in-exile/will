3ddc355501230c31ad9d55684eb654c800b928c3
June <df41022@gmail.com>
2025-07-09 06:20:03 +0800

    remove useless test

--- zkp/tests/substituteBytes.test.ts
+++ zkp/tests/substituteBytes.test.ts
@@ -33,7 +33,7 @@ describe("SubWord Circuit", function () {
 describe("SubstituteBytes Circuit", function () {
   let circuit: WitnessTester<["_in"], ["_out"]>;
 
-  describe("Byte Substitution", function (): void {
+  describe("Single Byte Substitution", function (): void {
     beforeAll(async function (): Promise<void> {
       circuit = await WitnessTester.construct(
         "circuits/shared/components/aes256gcm/substituteBytes.circom",
@@ -43,14 +43,14 @@ describe("SubstituteBytes Circuit", function () {
         },
       );
       console.info(
-        "1-Byte SubstitutionBytes circuit constraints:",
+        "1-byte SubstitutionBytes circuit constraints:",
         await circuit.getConstraintCount(),
       );
     });
 
-    it("should substitute random byte according to SBOX", async function (): Promise<void> {
-      for (let i = 0; i < 3; i++) {
-        const _in = AESUtils.randomBytes(1),
+    it("should substitute all bytes according to SBOX", async function (): Promise<void> {
+      for (let byte = 0x00; byte <= 0xff; byte++) {
+        const _in = Buffer.from([byte]),
           _out = AESSbox.substituteBytes(_in);
 
         await circuit.expectPass(
@@ -71,7 +71,7 @@ describe("SubstituteBytes Circuit", function () {
         },
       );
       console.info(
-        "16-Byte SubstitutionBytes circuit constraints:",
+        "16-byte SubstitutionBytes circuit constraints:",
         await circuit.getConstraintCount(),
       );
     });
@@ -86,32 +86,6 @@ describe("SubstituteBytes Circuit", function () {
           { _out: Array.from(_out) },
         );
       }
-    });
-  });
-
-  describe("256-Byte Bytes Substitution", function (): void {
-    beforeAll(async function (): Promise<void> {
-      circuit = await WitnessTester.construct(
-        "circuits/shared/components/aes256gcm/substituteBytes.circom",
-        "SubstituteBytes",
-        {
-          templateParams: ["256"],
-        },
-      );
-      console.info(
-        "256-Byte SubstitutionBytes circuit constraints:",
-        await circuit.getConstraintCount(),
-      );
-    });
-
-    it("should substitute all bytes in SBOX correctly", async function (): Promise<void> {
-      const _in = new Array(0),
-        _out = new Array(0);
-      for (let byte = 0x00; byte <= 0xff; byte++) {
-        _in.push(byte);
-        _out.push(AESSbox.SBOX[byte]);
-      }
-      await circuit.expectPass({ _in }, { _out });
     });
   });
 });
